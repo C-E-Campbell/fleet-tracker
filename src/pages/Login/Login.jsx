33e8b4292038523f7cf23e6cdb52da53b38ca932
charles <codelyfewithcharlie@gmail.com>
2019-12-26 18:18:36 -0500

    updates

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
 import "./Login.scss";
 
-import landingJPG from "../../assets/landing.jpg";
 const Login = props => {
-  const { userID, setUserID } = useState("");
-  const { pass, setPass } = useState("");
+  const [userID, setUserID] = useState("");
+  const [pass, setPass] = useState("");
+
+  const formHandler = (id, pass, e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="login-background">
       <div className="landing-jpg"></div>
-      <h1>FleetTrak</h1>
-      <form className="landing-login-form">
-        <input type="text" placeholder="Employee ID" />
-        <input type="password" placeholder="Password" />
+      <h1>FleetTrak&#8482;</h1>
+      <form
+        onSubmit={e => formHandler(userID, pass, e)}
+        className="landing-login-form"
+      >
+        <input
+          onChange={e => setUserID(e.target.value)}
+          type="text"
+          placeholder="Employee ID"
+          value={userID}
+        />
+        <input
+          onChange={e => setPass(e.target.value)}
+          type="password"
+          placeholder="Password"
+          value={pass}
+        />
         <button type="submit">Login</button>
       </form>
+      <h6>Create a maintenance group </h6>
     </div>
   );
 };
