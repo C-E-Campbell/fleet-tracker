e0f203bf9e9ba322eda199d3040d57127a49056b
charles <codelyfewithcharlie@gmail.com>
2020-01-01 14:00:31 -0500

    addd router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { BroswerRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/dasboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
