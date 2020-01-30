2fa2a3961d89bd144cc7195884b787cb571d2807
charles <codelyfewithcharlie@gmail.com>
2020-01-30 16:20:45 -0500

    changed css

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import "./Dashboard.scss";
 import VehicleCard from "../../components/VehicleCard/VehicleCard";
